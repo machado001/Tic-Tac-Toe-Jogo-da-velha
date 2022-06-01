ff852b615639be06dd25c355b3f92e90d5fcba69
Gabriel Machado <machadowg@gmail.com>
2022-06-01 00:09:18 -0300

    criando forma
    tabuleiro em construção

--- interface.js
+++ interface.js
@@ -1,11 +1,11 @@
 (function buildSquare() {
     const body = document.querySelector('body')
+    const main = document.createElement('main')
+    body.insertAdjacentElement("afterbegin",main)
     for (let i = 1; i <= 9; i++) {
         const square = document.createElement('div')
-        console.log(square)
         square.className = 'square'
         square.id = i
-        const addDiv = body.insertAdjacentElement('afterbegin',square)
+        main.insertAdjacentElement('beforeend',square)
     }
-
 }())
